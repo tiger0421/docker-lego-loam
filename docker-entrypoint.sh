ce9f5b55b4224ade0ad0cddae716ae6991dd2690
tiger0421 <taiger0421@yahoo.co.jp>
2020-05-27 01:27:16 +0900

    fix

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,7 +3,7 @@
 curl https://raw.githubusercontent.com/tiger0421/docker-lego-loam/master/install.sh > /tmp/install.sh
 chmod +x /tmp/install.sh
 
-sh /tmp/install.sh
+bash /tmp/install.sh
 rm /tmp/install.sh
 echo '#!/bin/sh' > /tmp/docker-entrypoint.sh
 echo 'bash' >> /tmp/docker-entrypoint.sh
