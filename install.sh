cecc3b1f4c9f4d02d7c76dad1e8cb6069630abef
tiger0421 <taiger0421@yahoo.co.jp>
2020-05-27 00:58:28 +0900

    first commit

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+source /opt/ros/$ROS_DISTRO/setup.bash
+
+# create workspace
+mkdir -p ~/catkin_ws/src
+cd ~/catkin_ws/src
+catkin_init_workspace
+cd ~/catkin_ws
+catkin build
+
+# clone LeGO-LOAM
+cd ~/catkin_ws/src
+git clone https://github.com/RobustFieldAutonomyLab/LeGO-LOAM.git
+cd ..
+catkin build
