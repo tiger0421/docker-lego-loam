05f49edfab5a555963b5a8a9e7d7d906cbb4b298
Hiroki Shimada <taiger0421@yahoo.co.jp>
2020-05-27 17:02:29 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ catkin build
 
 # clone LeGO-LOAM
 cd ~/catkin_ws/src
-https://github.com/facontidavide/LeGO-LOAM-BOR.git
+git clone https://github.com/facontidavide/LeGO-LOAM-BOR.git
 git clone https://github.com/koide3/ndt_omp
 git clone https://github.com/koide3/odometry_saver.git
 cd ..
